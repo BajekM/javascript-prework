5575a1476a27f555b1f53db27ba1ec34b4b8f151
Marcin Bajek <bajekmarcin@gmail.com>
2019-09-06 17:25:49 +0200

    Poprawione zadanie

--- js/script.js
+++ js/script.js
@@ -73,8 +73,14 @@ function playGame(playerInput) {
     //console.log('Wywołanie funkcji wyświetlającej wynik');
 }
 
-document.getElementById('play-rock').addEventListener('click', playGame(2));
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+  });
 
-document.getElementById('play-paper').addEventListener('click', playGame(2));
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+  });
 
-document.getElementById('play-scissors').addEventListener('click', playGame(3));
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+  });
